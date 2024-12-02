9a5e359864368cbf31d8e220128740b31415cde5
Micah <mjmikes@byu.edu>
2024-12-02 13:52:20 -0700

    removing intex-env link https

--- .platform/hooks/postdeploy/00_get_certificate.sh
+++ .platform/hooks/postdeploy/00_get_certificate.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
 # Place in .platform/hooks/postdeploy directory
 sudo certbot -n -d turtle.is404.net --nginx --agree-tos --email mjmikes@byu.edu
-sudo certbot -n -d http://intex-env.eba-bvpbbmfm.us-east-1.elasticbeanstalk.com/ --nginx --agree-tos --email mjmikes@byu.edu
