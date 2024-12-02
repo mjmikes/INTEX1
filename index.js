44d4025e30af0e7d405a181ef5ea023afe9782f9
Micah <mjmikes@byu.edu>
2024-12-01 20:04:48 -0700

    added public directory and static route for future css, js, and image changes

--- index.js
+++ index.js
@@ -13,16 +13,24 @@ app.use(express.urlencoded({extended: true}));
 let knex = require("knex")({
     client: "pg",
     connection: {
-        host: "localhost",
-        user: "postgres",
-        password: "admin1",
-        database: "INTEX1",
-        port: 5433
+        host: process.env.RDS_HOSTNAME || "localhost",
+        user: process.env.RDS_USERNAME || "postgres",
+        password: process.env.RDS_PASSWORD || "admin1",
+        database: process.env.RDS_DB_NAME || "INTEX1",
+        port: process.env.RDS_PORT || 5433,
+        ssl: process.env.DB_SSL ? { rejectUnauthorized: false } : false
+    },
+    pool: {
+        min: 2,
+        max: 15 // Increased max pool size to handle more connections
     }
 });
 
 app.get("/", (req,res) => {
     res.render("index.ejs")
 });
+
+// Serve static files from the "public" directory
+app.use(express.static('public'));
 
 app.listen(port, () =>console.log(`Server is listening on port ${port}`))
