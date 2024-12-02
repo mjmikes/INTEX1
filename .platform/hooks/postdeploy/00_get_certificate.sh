fde59c0d5835662d69e155ed81204a2a6a550b0b
Micah <mjmikes@byu.edu>
2024-12-02 13:22:07 -0700

    adding https and gitignore

--- .platform/hooks/postdeploy/00_get_certificate.sh
+++ .platform/hooks/postdeploy/00_get_certificate.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 # Place in .platform/hooks/postdeploy directory
-# sudo certbot -n -d pokemon.is404.net --nginx --agree-tos --email mjmikes@byu.edu
+sudo certbot -n -d turtle.is404.net --nginx --agree-tos --email mjmikes@byu.edu
+sudo certbot -n -d intex-env.eba-bvpbbmfm.us-east-1.elasticbeanstalk.com --nginx --agree-tos --email mjmikes@byu.edu
