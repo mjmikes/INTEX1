2573331ac3543b96a1bd1ca728f21fc8319b88f6
johnnyt16 <johntaylormcc@gmail.com>
2024-12-03 15:28:51 -0700

    event request post route

--- index.js
+++ index.js
@@ -27,6 +27,7 @@ const knex = require("knex")({
     }
 });
 
+// GET ROUTES TO ACCESS PAGES
 
 // get route for the landing page (index.ejs1)
 app.get("/", (req,res) => {
@@ -60,5 +61,76 @@ app.get('/donate', (req, res) => {
 
 // Serve static files from the "public" directory
 app.use(express.static('public'));
+
+
+// POST ROUTES TO UPDATE DATA
+
+app.post("/addEventRequest", (req, res) => {
+    const {
+        event_name, event_contact_first_name, event_contact_last_name,
+        event_contact_phone, event_contact_email, event_type, event_location_address,
+        event_location_city, event_location_state, event_location_zip, event_start_time,
+        event_duration, event_description, expected_advanced_sewers, sewing_machines_available,
+        expected_participants, children_under_10, jen_story, event_space_description,
+        round_tables, rectangle_tables, possible_date_1, possible_date_2
+    } = req.body;
+
+    // Insert into event_contact table
+    knex('event_contact')
+      .returning('event_contact_id')
+      .insert({
+        event_contact_first_name: event_contact_first_name,
+        event_contact_last_name: event_contact_last_name,
+        event_contact_phone: event_contact_phone,
+        event_contact_email: event_contact_email
+      })
+      .then(eventContactIds => {
+        const eventContactId = eventContactIds[0];  // Get the ID of the newly inserted event contact
+
+        // Insert into event_location table
+        return knex('event_location')
+          .returning('event_location_id')
+          .insert({
+            event_address: event_location_address,
+            event_city: event_location_city,
+            event_state: event_location_state,
+            event_zip: event_location_zip
+          }).then(eventLocationIds => {
+            const eventLocationId = eventLocationIds[0];  // Get the ID of the newly inserted event location
+
+            // Now insert into event_request table
+            return knex('event_request').insert({
+                event_name: event_name,
+                event_contact_id: eventContactId, // Foreign Key
+                event_type: event_type,
+                event_location_id: eventLocationId, // Foreign Key
+                event_start_time: event_start_time,
+                event_duration: event_duration,
+                event_description: event_description,
+                expected_advanced_sewers: expected_advanced_sewers,
+                sewing_machines_available: sewing_machines_available,
+                expected_participants: expected_participants,
+                children_under_10: children_under_10,
+                jen_story: jen_story,
+                event_space_description: event_space_description,
+                round_tables: round_tables,
+                rectangle_tables: rectangle_tables,
+                possible_date_1: possible_date_1,
+                possible_date_2: possible_date_2
+            }).then(() => {
+                // After inserting data, send redirect to send a success message
+                res.redirect('/event_success_page');  
+            }).catch(error => {
+                console.error('Error inserting event_request:', error);
+                res.status(500).send('Internal Server Error');
+            });
+        });
+      })
+      .catch(error => {
+        console.error('Error inserting event_contact or event_location:', error);
+        res.status(500).send('Internal Server Error');
+      });
+});
+
 
 app.listen(port, () =>console.log(`Server is listening on port ${port}!`))
