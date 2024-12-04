474c798045bc998f2c206b8d4240a52f0f5caa53
Mckay <kimballmckay12@gmail.com>
2024-12-04 16:01:26 -0700

    volunteers

--- index.js
+++ index.js
@@ -485,7 +485,7 @@ app.get('/volunteers', (req, res) => {
         .then(volunteer_info => {
                 // Render the index.ejs template and pass the data
                 // We use res.render to work with ejs files we use res.redirct to work with routes
-                res.render('volunteer_info', { volunteer_info, security });
+                res.render('volunteers', { volunteer_info, security });
         })
             .catch(error => {
                 console.error('Error querying database:', error);
